3529de991ca3279c37ac30d89cc6d7388717f385
Bjorn Loyens <bjorn.loyens@gmail.com>
2021-05-27 14:58:46 +0200

    html, css and js adjusted - final git for desktop - no responsive mode

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 const divs = [...document.querySelectorAll('.question')];
 console.log(divs);
 
-function toggleClass(){
+
     divs.map(div => {
-        const children = [...div.children];
-        console.log(children);
         div.addEventListener('click', ()=> {
             const question = div.children[0];
             const icon = div.children[1];
             const answer = div.children[2];
-            question.classList.toggle('--view');
-            icon.classList.toggle('--view');
-            answer.classList.toggle('--view');
-        })
-    })
-}
+                question.classList.toggle('question__title--view');
+                icon.classList.toggle('question__icon--view');
+                answer.classList.toggle('question__answer--view');
 
-toggleClass();
+            
+        })})
